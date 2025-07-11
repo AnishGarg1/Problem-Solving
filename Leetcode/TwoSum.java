83c0900593d8e32bec9ce2f738bd13a134746ee3
AnishGarg1 <anishgarg805@gmail.com>
2025-07-11 23:35:03 +0530

    optimzed Two Sum

--- Leetcode/TwoSum.java
+++ Leetcode/TwoSum.java
@@ -1,12 +1,22 @@
+import java.util.HashMap;
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         int n = nums.length;
+        // for (int i = 0; i < n; i++) {
+        // for (int j = i + 1; j < n; j++) {
+        // if (nums[i] + nums[j] == target) {
+        // return new int[] { i, j };
+        // }
+        // }
+        // }
+
+        HashMap<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if (nums[i] + nums[j] == target) {
-                    return new int[] { i, j };
-                }
+            if (map.containsKey(target - nums[i])) {
+                return new int[] { map.get(target - nums[i]), i };
             }
+            map.put(nums[i], i);
         }
 
         return new int[] { 0, 0 };
